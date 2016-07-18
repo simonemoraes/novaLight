64f835e250d71b91cf556ac13ec708852e356423
Simone <Simone@Simone-PC>
2016-07-18 16:45:28 -0300

    Modificações feitas em 18/07/16

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@
 |
 */
 $config['base_url'] = 'http://localhost:8080/nova_light';
-
+//$config['base_url'] = 'http://simone.ruianderson.com.br/nova_light';
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -28,6 +28,7 @@ $config['base_url'] = 'http://localhost:8080/nova_light';
 | variable so that it is blank.
 |
 */
+//$config['index_page'] = 'index.php';
 $config['index_page'] = 'index.php';
 
 /*
